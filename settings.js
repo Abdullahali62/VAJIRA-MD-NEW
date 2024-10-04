ffc783b29dace51dbbed0dfb00ff778bb7736a0f
Abdullahali62 <salefor472@gmail.com>
2024-10-04 16:21:56 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=6J9nHBzL#ACl7ZTIS18OXuNS-6J1CKqMbN83xz9zUYhmChgKuxR8' : process.env.SESSION_ID,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '923168721889' : process.env.OWNER_NUMBER,   
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=WNs3ja6R#LPpRT8AxxGIFiTW2l9S0OsSzEb7wBKuJTa1HncsMAEU' : process.env.SESSION_ID,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '923163154307' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
 AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || false  ,    
